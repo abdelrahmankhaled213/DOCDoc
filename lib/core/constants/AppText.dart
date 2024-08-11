d4751495f263de152bce80f13c3f76a568611f4d
abdelrahman <bodeekhaled9@gmail.com>
2024-08-11 21:59:40 +0200

    Adding App Text

---
+++ lib/core/constants/AppText.dart
@@ -0,0 +1,7 @@
+abstract class AppText{
+static const String appName="DocDoc";
+static const String overView="Best Doctor\n Appointment App";
+static const String caption="Manage and schedule all of your medical appointments easily with Docdoc to get a new experience";
+static const String getStarted="Get Started";
+
+}
