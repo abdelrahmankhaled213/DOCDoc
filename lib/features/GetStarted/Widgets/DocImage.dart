38964cd214b79d97710e81a3cc759ffe71cabe01
abdelrahman <bodeekhaled9@gmail.com>
2024-08-11 22:07:34 +0200

    Adding Doc Image

---
+++ lib/features/GetStarted/Widgets/DocImage.dart
@@ -0,0 +1,29 @@
+import 'package:docdoc/core/constants/AppImage.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+class DocImage extends StatelessWidget {
+  const DocImage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      foregroundDecoration: BoxDecoration(
+        gradient: LinearGradient(
+            colors: [
+              Colors.white,
+              Colors.white.withOpacity(0.0),
+            ],
+            begin: Alignment.bottomCenter,
+            end: Alignment.topCenter,
+            stops: const [
+              0.14
+              ,0.4
+            ]
+        ),
+      ),
+      child: Image.asset(
+          AppImage.Doc,
+      ),
+    );
+  }
+}
