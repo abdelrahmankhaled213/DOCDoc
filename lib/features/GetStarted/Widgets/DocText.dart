d47b77b803eea881abb691e7872b50346d8bbbbc
abdelrahman <bodeekhaled9@gmail.com>
2024-08-11 22:10:08 +0200

    Adding Doc Text Widget

---
+++ lib/features/GetStarted/Widgets/DocText.dart
@@ -0,0 +1,20 @@
+import 'package:docdoc/core/constants/AppStyle.dart';
+import 'package:docdoc/core/constants/AppText.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+class DocText extends StatelessWidget {
+  const DocText({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding:  EdgeInsets.symmetric(horizontal: 15.w),
+      child: Text(AppText.overView,
+        style: AppStyle.w700Interbluebold25.copyWith(
+          height: 1.5,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+}
