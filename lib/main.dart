f43f9fbd2455c514ca9103c742450e18453a256c
abdelrahman <bodeekhaled9@gmail.com>
2024-08-15 02:34:44 +0200

    our main

---
+++ lib/main.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import 'core/routing/approuter.dart';
+
+void main(){
+  runApp(DocDoc());
+}
+class DocDoc extends StatelessWidget {
+
+
+  @override
+  Widget build(BuildContext context) {
+    return ScreenUtilInit(
+      builder: (_, child) {
+        return MaterialApp.router(
+          debugShowCheckedModeBanner: false,
+          title: "DocDoc",
+          routerConfig: gorouter,
+
+        );
+      },
+      designSize: Size(360, 690),
+
+    );
+  }
+}
