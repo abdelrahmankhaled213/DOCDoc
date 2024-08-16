275460dd781b51b913835b420f63957306b78b30
abdelrahman <bodeekhaled9@gmail.com>
2024-08-17 01:55:01 +0200

    some changes

--- lib/features/GetStarted/Widgets/DocCaption.dart
+++ lib/features/GetStarted/Widgets/DocCaption.dart
@@ -1,5 +1,6 @@
 import 'package:docdoc/core/constants/AppStyle.dart';
 import 'package:docdoc/core/constants/AppText.dart';
+import 'package:docdoc/core/routing/approuter.dart';
 import 'package:docdoc/core/widgets/CustomButton.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
@@ -11,14 +12,16 @@ class DocCaptionButton extends StatelessWidget {
       child: Column(
         children: [
           Text(AppText.caption,
-            style: AppStyle.w400Intergrey15,
+            style: AppStyle.w400InterGrey15,
             textAlign: TextAlign.center,
           ),
           SizedBox(height: 15.h,),
 
           Padding(
             padding:  EdgeInsets.all(8.0.sp),
-            child: CustomButton(text: AppText.getStarted, onpressed: (){}),
+            child: CustomButton(text: AppText.getStarted, onpressed: (){
+              pushAndRemoveUntilScreen(context: context, path: AppRoute.login);
+            }),
           ),
 
         ],
