45dba1dc9715c85abf28611876399a2ebe820a87
abdelrahman <bodeekhaled9@gmail.com>
2024-08-20 04:39:25 +0200

    Adding api constants and api errors

---
+++ lib/core/networking/api_constants.dart
@@ -0,0 +1,21 @@
+class ApiConstants{
+  static const  String baseUrl = "https://vcare.integration25.com/api/";
+  static const  String endPointLogin = "auth/login";
+}
+class ApiErrors{
+static const String noContent = "No response from server";
+static const String badRequest = "Bad request";
+static const String unauthorised = "Unauthorised";
+static const String notFound = "Not found";
+static const String forbidden = "Forbidden";
+static const String internalServerError = "Internal server error";
+static const String connectTimeout = "Connect timeout";
+static const String cancel = "Cancel";
+static const String receiveTimeout = "Receive timeout";
+static const String sendTimeout = "Send timeout";
+static const String cacheError = "Cache error";
+static const String noInternetConnection = "No internet connection";
+static const String defaultError = "Something went wrong, try again later";
+static const String cancelError = "Request to API server was cancelled";
+}
+
