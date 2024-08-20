06bd25599b07774d18611980bb9e781fcc8c6a2a
abdelrahman <bodeekhaled9@gmail.com>
2024-08-20 04:46:17 +0200

    changes

--- lib/features/LoginScreen/Ui/login_screen.dart
+++ lib/features/LoginScreen/Ui/login_screen.dart
@@ -1,36 +1,41 @@
 import 'package:docdoc/core/constants/AppStyle.dart';
 import 'package:docdoc/core/constants/AppText.dart';
 import 'package:docdoc/core/helpers/spacing.dart';
-import 'package:docdoc/features/LoginScreen/widgets/sign_up_form.dart';
+import 'package:docdoc/features/LoginScreen/logic/login_cubit.dart';
+import 'package:docdoc/features/LoginScreen/widgets/signIn.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class LoginScreenView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    context.read<LoginCubit>();
     return Scaffold(
       body: SafeArea(
           child: Padding(
             padding:  EdgeInsets.symmetric(vertical: 20.h,horizontal: 20.w),
-            child: ListView(
-                    children: [
+            child: SingleChildScrollView(
+              child: Column(
+                      children: [
 
-            Text(AppText.welcomeBack,style: AppStyle.w700InterBlue17.copyWith(
-              fontSize: 24.sp
-            ),),
+              Text(AppText.welcomeBack,style: AppStyle.w700InterBlue17.copyWith(
+                fontSize: 24.sp
+              ),),
 
-verticalSpace(10),
+              verticalSpace(10),
 
-            Text(AppText.loginText,style: AppStyle.w400InterGrey15.copyWith(
-              fontSize: 10.sp
-            )
+              Text(AppText.loginText,style: AppStyle.w400InterGrey15.copyWith(
+                fontSize: 10.sp
+              )
+              ),
+                        verticalSpace(20),
+                        SignInForm(),
+
+                      ],
+                    ),
             ),
-                      verticalSpace(20),
-                      SignUpForm(),
-
-                    ],
-                  ),
           )
       ),
     );
