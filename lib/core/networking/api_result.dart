2b87ff67a3e0cfc4a79df514242215646d1bb499
abdelrahman <bodeekhaled9@gmail.com>
2024-08-20 04:29:42 +0200

    Adding api_result class

---
+++ lib/core/networking/api_result.dart
@@ -0,0 +1,14 @@
+import 'package:docdoc/core/networking/api_error_handler.dart';
+import 'package:docdoc/core/networking/api_error_model.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'api_result.freezed.dart';
+
+@Freezed()
+abstract class ApiResult<T> with _$ApiResult<T>  {
+  const factory ApiResult.success(T data) = Success<T>;
+  const factory ApiResult.failure({required ErrorHandler error}) =
+      Failure<T>;
+
+}
+
