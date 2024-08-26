fdfe7e9806885e00fddaaf791b4299ef1f3c0439
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2024-08-26 04:52:41 +0300

    changes in import from main.dart to main_dev.dart

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -9,7 +9,7 @@ import 'package:docdoc/DocDoc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:docdoc/main.dart';
+import 'package:docdoc/main_dev.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
