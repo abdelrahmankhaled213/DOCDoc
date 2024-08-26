3232592e039e6b7461aa7926743b23579d12ac1a
LAPTOP-N2P6TMJ2\lenovo <abdelrahmanseif070@gmail.com>
2024-08-26 04:54:08 +0300

    creating main_dev.dart

---
+++ lib/main_dev.dart
@@ -0,0 +1,45 @@
+import 'package:docdoc/DocDoc.dart';
+import 'package:docdoc/core/Di/dependency%20injection.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+
+void main()async{
+WidgetsFlutterBinding.ensureInitialized();
+await ScreenUtil.ensureScreenInitialized();
+  Bloc.observer= MyBlocObserver();
+  await setupServiceLocator();
+  runApp(
+ DocDoc(),
+  );
+}
+
+
+
+
+class MyBlocObserver extends BlocObserver {
+  @override
+  void onCreate(BlocBase bloc) {
+    super.onCreate(bloc);
+    print('onCreate -- ${bloc.runtimeType}');
+  }
+
+  @override
+  void onChange(BlocBase bloc, Change change) {
+    super.onChange(bloc, change);
+    print('onChange -- ${bloc.runtimeType}, $change');
+  }
+
+  @override
+  void onError(BlocBase bloc, Object error, StackTrace stackTrace) {
+    print('onError -- ${bloc.runtimeType}, $error');
+    super.onError(bloc, error, stackTrace);
+  }
+
+  @override
+  void onClose(BlocBase bloc) {
+    super.onClose(bloc);
+    print('onClose -- ${bloc.runtimeType}');
+  }
+}
